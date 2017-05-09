0cdbfec21fada54b177a7e0fb8d708bb383c0290
Kenneth Lewenhagen <lewenhagen@live.com>
2017-05-09 23:05:20 +0200

    further development...

--- dependencies.md
+++ dependencies.md
@@ -6,3 +6,7 @@ sudo apt-get install libtiff4-dev libjpeg8-dev zlib1g-dev \
     libfreetype6-dev liblcms2-dev libwebp-dev tcl8.5-dev tk8.5-dev
 
 sudo pip3 install Pillow
+
+
+### Python-magic
+sudo pip3 install python-magic
--- functions.py
+++ functions.py
@@ -5,6 +5,28 @@ Functions file
 """
 
 import os
+from PIL import Image
+import re
+# from PythonMagick import Image
+
+# from PIL.ExifTags import TAGS
+import imghdr
+import magic
+
+MONTHS = {
+    "01": "Januari",
+    "02": "Februari",
+    "03": "Mars",
+    "04": "April",
+    "05": "Maj",
+    "06": "Juni",
+    "07": "Juli",
+    "08": "Augusti",
+    "09": "September",
+    "10": "Oktober",
+    "11": "November",
+    "12": "December",
+}
 
 def presentFolders(FOLDERS):
 
@@ -59,9 +81,37 @@ def scan_folder(FOLDERS, scan_me):
         print(*structure, sep="\n")
 
 
+def get_exif(fn):
+    created = {"year": "NA", "month": "NA"}
+    try:
+        match = re.search("([0-9]*):([0-9]*)", Image.open(fn)._getexif()[306])
+        created["year"] = match.group(1)
+        created["month"] = MONTHS[match.group(2)]
+
+    except TypeError:
+        pass
+
+    return created
+
+
 def startSort(FOLDERS):
     """ Initiates the sort """
+    images = []
+    videos = []
+    mime = magic.Magic(mime=True)
+
     if not os.listdir(FOLDERS["unsortedFolder"]):
         print("empty!")
     else:
-        print("Not empty!") 
+        for dirname, dirnames, filenames in os.walk(FOLDERS["unsortedFolder"]):
+            for filename in filenames:
+                if "image" in mime.from_file(os.path.join(dirname, filename)):
+                    images.append({
+                            "image": os.path.join(dirname, filename),
+                            "created": get_exif(os.path.join(dirname, filename))
+                        })
+                if "video" in mime.from_file(os.path.join(dirname, filename)):
+                    videos.append({"video": os.path.join(dirname, filename)})
+
+    print(images)
+    print(videos)
